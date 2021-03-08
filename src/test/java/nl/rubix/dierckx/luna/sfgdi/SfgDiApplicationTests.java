8735f96a074f866d69920796b47db35002d062ce
Luna Dierckx <luna.dierckx@rubix.nl>
2021-03-08 12:06:29 +0100

    initial commit

---
+++ src/test/java/nl/rubix/dierckx/luna/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rubix.dierckx.luna.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
