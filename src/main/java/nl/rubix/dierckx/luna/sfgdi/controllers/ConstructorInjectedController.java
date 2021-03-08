61fb50ee0570240f0bb517035e4cf0dd0b7d2f63
Luna Dierckx <luna.dierckx@rubix.nl>
2021-03-08 13:06:44 +0100

    using qualifiers with Spring Injection

--- src/main/java/nl/rubix/dierckx/luna/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/nl/rubix/dierckx/luna/sfgdi/controllers/ConstructorInjectedController.java
@@ -2,6 +2,7 @@ package nl.rubix.dierckx.luna.sfgdi.controllers;
 
 import nl.rubix.dierckx.luna.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,7 +10,7 @@ public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
