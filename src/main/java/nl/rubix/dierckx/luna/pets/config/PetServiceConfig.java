68f7b4af70a8780530787065571b0ae8610d7ba6
LunaDierckx <luna.dierckx@rubix.nl>
2021-04-16 18:10:00 +0200

    pet service factory + pet service factory bean config

---
+++ src/main/java/nl/rubix/dierckx/luna/pets/config/PetServiceConfig.java
@@ -0,0 +1,30 @@
+package nl.rubix.dierckx.luna.pets.config;
+
+import nl.rubix.dierckx.luna.pets.CatPetService;
+import nl.rubix.dierckx.luna.pets.DogPetService;
+import nl.rubix.dierckx.luna.pets.PetService;
+import nl.rubix.dierckx.luna.pets.PetServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class PetServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory() {
+        return new PetServiceFactory();
+    }
+
+    @Profile({"cat", "default"})
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("cat");
+    }
+
+    @Profile("dog")
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("dog");
+    }
+}
