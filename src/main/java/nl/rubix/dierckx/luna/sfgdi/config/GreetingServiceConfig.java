47c2c3df346209c91e7ae0dfa0e2afc57aa7659e
LunaDierckx <luna.dierckx@rubix.nl>
2021-04-17 17:34:53 +0200

    use application.properties

--- src/main/java/nl/rubix/dierckx/luna/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/nl/rubix/dierckx/luna/sfgdi/config/GreetingServiceConfig.java
@@ -7,7 +7,6 @@ import nl.rubix.dierckx.luna.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
