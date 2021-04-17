cda022e2e338dfc85d779aab3b0a87eed27fd57e
LunaDierckx <luna.dierckx@rubix.nl>
2021-04-17 17:22:48 +0200

    using properties source

--- src/main/java/nl/rubix/dierckx/luna/sfgdi/SfgDiApplication.java
+++ src/main/java/nl/rubix/dierckx/luna/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package nl.rubix.dierckx.luna.sfgdi;
 
 import nl.rubix.dierckx.luna.sfgdi.controllers.*;
+import nl.rubix.dierckx.luna.sfgdi.datasource.FakeDataSource;
 import nl.rubix.dierckx.luna.sfgdi.services.PrototypeBean;
 import nl.rubix.dierckx.luna.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
@@ -49,6 +50,11 @@ public class SfgDiApplication {
         System.out.println(prototypeBean1.getMyScope());
         PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
         System.out.println(prototypeBean2.getMyScope());
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUsername());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getJdbcurl());
     }
 
 }
--- src/main/java/nl/rubix/dierckx/luna/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/nl/rubix/dierckx/luna/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,27 @@
 package nl.rubix.dierckx.luna.sfgdi.config;
 
+import nl.rubix.dierckx.luna.sfgdi.datasource.FakeDataSource;
 import nl.rubix.dierckx.luna.sfgdi.repositories.EnglishGreetingRepository;
 import nl.rubix.dierckx.luna.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import nl.rubix.dierckx.luna.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
+@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
+                                  @Value("${guru.password}") String password,
+                                  @Value("${guru.jdbcurl}") String jdbcurl) {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+        return fakeDataSource;
+    }
 
     @Primary
     @Bean
